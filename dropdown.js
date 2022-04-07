27817ee48e6bed917ad4c28ce3244d8638fda8c2
Jadson Neres <jadsonribeironeres@hotmail.com>
2022-04-07 12:46:48 -0300

    Estilização dos SubMenus

--- dropdown.js
+++ dropdown.js
@@ -1,10 +1,17 @@
-const drop = document.getElementById('navbarDropdown')
-console.log(drop)
+const menu = document.getElementById('navbarDropdownMenu')
+const login = document.getElementById('navbarDropdownCadastro')
 
 const subMenu = document.querySelector('.dropdown-menu')
+const subCadastro = document.querySelector('.Cadastro')
 
-drop.addEventListener('click', toggle)
+menu.addEventListener('click', toggle)
+login.addEventListener('click', toggle2)
+
 function toggle(e){
     subMenu.classList.toggle('toggle__menu')
     
+    
+}
+function toggle2(e){
+    subCadastro.classList.toggle('toggle__cadastro')
 }
